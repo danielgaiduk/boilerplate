0107f79cad57fe2e0d9978eb234faec257ff03e6
Daniel Gaiduk <daniel@gaiduk.dev>
2023-08-08 01:23:06 +0200

    Add locale and translations

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,8 +1,15 @@
+import {
+	buildLocalizedUrl,
+	isLocaleAvailable,
+	log,
+	parseCookie,
+	redirect,
+	replaceHTMLFragments
+} from '$lib/utils'
+import { PUBLIC_SENTRY_DSN } from '$env/static/public'
 import { sequence } from '@sveltejs/kit/hooks'
 import * as Sentry from '@sentry/sveltekit'
 import type { Handle } from '@sveltejs/kit'
-
-import { PUBLIC_SENTRY_DSN } from '$env/static/public'
 
 Sentry.init({
 	dsn: PUBLIC_SENTRY_DSN,
@@ -10,7 +17,38 @@ Sentry.init({
 })
 
 const handle = sequence(Sentry.sentryHandle(), (async ({ event, resolve }) => {
-	return await resolve(event)
+	log('SERVER HOOK CALLED')
+
+	const {
+		locals,
+		params: { locale },
+		request,
+		route: { id },
+		url
+	} = event
+
+	if (id && !id?.includes('(localized)')) {
+		return await resolve(event)
+	}
+
+	const cookie = parseCookie(request)
+
+	if (!isLocaleAvailable(locale)) {
+		const location = buildLocalizedUrl(cookie, request, url)
+
+		return redirect(location)
+	}
+
+	locals.locale = locale
+
+	const response = await resolve(
+		event,
+		replaceHTMLFragments({
+			'%lang%': `lang="${locale}"`
+		})
+	)
+
+	return response
 }) satisfies Handle)
 
 const handleError = Sentry.handleErrorWithSentry()
