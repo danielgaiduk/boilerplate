c94e5f5fa839b830d367c68aa3162d15bd19564e
Daniel Gaiduk <daniel@gaiduk.dev>
2023-08-08 01:21:34 +0200

    Rewrite index page

---
+++ src/routes/(localized)/[[locale]]/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+	import { t } from '$lib/translations'
+</script>
+
+<h1 class="text-6xl uppercase">{$t('app.app_name')}</h1>
--- src/routes/+page.svelte
+++
@@ -1,8 +0,0 @@
-<h1 class="text-4xl text-red-500">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
-<div class="@container">
-    <div class="@lg:text-sky-400">
-        test
-      </div>
-</div>
