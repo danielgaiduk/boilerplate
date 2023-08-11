641894f1769d1fe424945bb6785aad5cf373c0d8
Daniel Gaiduk <daniel@gaiduk.dev>
2023-08-12 01:12:34 +0200

    Add temporal to build cookie function

--- src/lib/utils/cookie.ts
+++ src/lib/utils/cookie.ts
@@ -1,5 +1,5 @@
-import { APP_NAME } from '$lib/config/app.json'
-import { toKebabCase } from '$lib/utils'
+import dayjs from 'dayjs'
+import { KEBAB_APP_NAME } from '$lib/constants'
 
 function parseCookie(request: Request): Record<string, string> {
 	const cookie = request.headers.get('cookie')
@@ -19,10 +19,15 @@ function parseCookie(request: Request): Record<string, string> {
 	return cookies
 }
 
-function buildCookie(name: string, value: string) {
-	const formattedAppName = toKebabCase(APP_NAME)
+function buildCookie(
+	name: string,
+	value: string,
+	temporal: ITemporal = { amount: 1, unit: 'year' }
+) {
+	const { amount, unit } = temporal
+	const expires = dayjs().add(amount, unit).toDate().toUTCString()
 
-	return `${formattedAppName}_${name}=${value};path=/;secure=true;sameSite=strict;httpOnly=true;`
+	return `${KEBAB_APP_NAME}_${name}=${value};path=/;secure=true;sameSite=strict;httpOnly=true;expires=${expires}`
 }
 
 export { parseCookie, buildCookie }
