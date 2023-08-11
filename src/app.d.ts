b5b180db46fe868c2e97c90010dd691c3e42aaac
Daniel Gaiduk <daniel@gaiduk.dev>
2023-08-12 01:12:18 +0200

    Add pocketbase types to app.d.ts

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,8 +1,12 @@
+type Pocketbase = import('pocketbase').default
+
 declare global {
 	namespace App {
 		// interface Error {}
 		interface Locals {
 			locale?: string
+			pb?: Pocketbase
+			user?: Pocketbase.authStore.model
 		}
 		interface PageData {
 			locale?: string
