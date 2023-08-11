3b7053cc47287cc9d1b0043a876192e23663bb65
Daniel Gaiduk <daniel@gaiduk.dev>
2023-08-11 13:15:10 +0200

    Rewrite seo logic in +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,23 +1,38 @@
-<script>
+<script lang="ts">
 	import '../app.css'
 	import { DEFAULT_TITLE, DEFAULT_DESCRIPTION } from '$lib/config/app.json'
-	import { getAllLocalePaths } from '$lib/utils'
+	import { getAllLocalizedPaths } from '$lib/utils'
 	import { MetaTags } from 'svelte-meta-tags'
 	import { page } from '$app/stores'
 	import { t } from '$lib/translations'
 
+	let title = ''
+	let description = ''
+	let paths: IAlternateLinks[] = []
+	let seo: ISeoTemplate = {}
+
 	$: ({ data, url } = $page)
-	$: localePaths = getAllLocalePaths(data.locale, url)
-	$: title = `${data?.seo?.title ? `${$t(data?.seo?.title)} | ` : ''}${$t(DEFAULT_TITLE)}`
-	$: description = data?.seo?.description ? $t(data?.seo?.description) : $t(DEFAULT_DESCRIPTION)
+
+	$: if (data?.locale) {
+		title = $t(DEFAULT_TITLE)
+		description = $t(DEFAULT_DESCRIPTION)
+		seo.title = data.seo?.title ? $t(data.seo?.title) : title
+		seo.description = data.seo?.description ? $t(data.seo?.description) : description
+		paths = getAllLocalizedPaths(data?.locale, url)
+	}
 </script>
 
 <svelte:head>
-	{#each localePaths as { href, hreflang }}
+	{#each paths as { href, hreflang }}
 		<link rel="alternate" {hreflang} {href} />
 	{/each}
 </svelte:head>
 
-<MetaTags {title} {description} canonical={url.href} />
+<MetaTags
+	{title}
+	canonical={url.href}
+	description={seo.description}
+	titleTemplate={`${seo.title} | %s`}
+/>
 
 <slot />
