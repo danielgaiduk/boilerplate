4bf7ff4792fd9666c0fce28c8034f6ec96e611f4
Daniel Gaiduk <daniel@gaiduk.dev>
2023-08-15 17:25:44 +0200

    Add package.json version to environment

--- src/routes/(localized)/[[locale]]/+page.svelte
+++ src/routes/(localized)/[[locale]]/+page.svelte
@@ -1,5 +1,7 @@
 <script>
 	import { t } from '$lib/translations'
+	import { version } from '$app/environment'
 </script>
 
-<h1 class="text-6xl uppercase">{$t('app.name')}</h1>
+<h1 class="text-4xl uppercase">{$t('app.name')}</h1>
+<h2 class="text-xs uppercase">Version: {version}</h2>
--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,21 @@
 import adapter from '@sveltejs/adapter-node'
 import { vitePreprocess } from '@sveltejs/kit/vite'
+import { readFileSync } from 'fs'
+import { fileURLToPath } from 'url'
+
+const url = new URL('package.json', import.meta.url)
+const file = fileURLToPath(url)
+const json = readFileSync(file, 'utf8')
+const pkg = JSON.parse(json)
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		version: {
+			name: pkg.version
+		}
 	}
 }
 
