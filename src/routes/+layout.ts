c42aaab750d9ad0cda82e5021fd435d4807d6bb5
Daniel Gaiduk <daniel@gaiduk.dev>
2023-08-29 22:22:54 +0200

    Remove locale type

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -2,7 +2,7 @@ import { loadTranslations } from '$lib/translations'
 import type { LayoutLoad } from './$types'
 
 const load = (async ({ data: { locale } }) => {
-	await loadTranslations(locale as string)
+	await loadTranslations(locale)
 
 	return { locale }
 }) satisfies LayoutLoad
