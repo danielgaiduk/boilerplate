4cac215413b71de5f24a593f11daf80905b18d23
Daniel Gaiduk <daniel@gaiduk.dev>
2023-08-29 22:20:08 +0200

    Rewrite function names to kebab case

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,8 +1,8 @@
 import * as APP_CONFIG from '$lib/config/app.json'
 import * as TRANSLATION_CONFIG from '$lib/config/translation.json'
-import { toKebabCase } from '$lib/utils'
+import to_kebab_case from '$lib/utils/to_kebab_case'
 
-const KEBAB_APP_NAME = toKebabCase(APP_CONFIG.APP_NAME)
+const KEBAB_APP_NAME = to_kebab_case(APP_CONFIG.APP_NAME)
 const AUTH_COOKIE_NAME = `${KEBAB_APP_NAME}_auth`
 const LOCALE_COOKIE_NAME = `${KEBAB_APP_NAME}_locale`
 
