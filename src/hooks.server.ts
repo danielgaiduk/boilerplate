a5405dbcf63a4d65419c670f68dd2fa7d05cb236
Daniel Gaiduk <daniel@gaiduk.dev>
2023-08-29 22:20:18 +0200

    Rewrite hooks.server.ts

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,16 +1,14 @@
-import {
-	buildCookie,
-	buildLocalizedUrl,
-	isLocaleAvailable,
-	log,
-	parseCookie,
-	redirect,
-	replaceHTMLFragments
-} from '$lib/utils'
 import { PUBLIC_SENTRY_DSN } from '$env/static/public'
 import { sequence } from '@sveltejs/kit/hooks'
-import { setupPocketbase } from '$lib/server'
 import * as Sentry from '@sentry/sveltekit'
+import build_cookie from '$lib/utils/build_cookie'
+import build_localized_url from '$lib/utils/build_localized_url'
+import is_locale_available from '$lib/utils/is_locale_available'
+import log from '$lib/utils/log'
+import parse_cookie from '$lib/utils/parse_cookie'
+import redirect from '$lib/utils/redirect'
+import replace_html_fragments from '$lib/utils/replace_html_fragments'
+import setup_pocketbase from '$lib/server/setup_pocketbase'
 import type { Handle } from '@sveltejs/kit'
 
 Sentry.init({
@@ -21,27 +19,23 @@ Sentry.init({
 const handle = sequence(Sentry.sentryHandle(), (async ({ event, resolve }) => {
 	log('SERVER HOOK CALLED')
 
-	const {
-		locals,
-		params: { locale },
-		request,
-		route: { id },
-		url
-	} = event
+	const { params, request, route, url } = event
+	const { locale = '' } = params
+	const { id } = route
 
 	if (id && !id?.includes('(localized)')) {
 		return await resolve(event)
 	}
 
-	const cookie = parseCookie(request)
+	const cookie = parse_cookie(request)
 
-	if (!isLocaleAvailable(locale)) {
-		const location = buildLocalizedUrl(cookie, request, url)
+	if (!is_locale_available(locale)) {
+		const location = build_localized_url(cookie, request, url)
 
-		return redirect(location)
+		if (location) return redirect(location)
 	}
 
-	const pb = await setupPocketbase(cookie)
+	const pb = await setup_pocketbase(cookie)
 
 	if (pb.authStore.isValid) {
 		if (id?.includes('(unguarded)')) {
@@ -53,18 +47,20 @@ const handle = sequence(Sentry.sentryHandle(), (async ({ event, resolve }) => {
 		}
 	}
 
-	locals.locale = locale
-	locals.pb = pb
-	locals.user = structuredClone(pb.authStore.model)
+	event.locals = {
+		locale,
+		pb,
+		user: structuredClone(pb.authStore.model)
+	}
 
 	const response = await resolve(
 		event,
-		replaceHTMLFragments({
+		replace_html_fragments({
 			'%lang%': `lang="${locale}"`
 		})
 	)
 
-	response.headers.append('set-cookie', buildCookie('locale', locale as string))
+	response.headers.append('set-cookie', build_cookie('locale', locale))
 
 	return response
 }) satisfies Handle)
