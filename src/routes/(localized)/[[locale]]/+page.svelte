3177cb3ea4ee543644162fca644446dc40c6138e
Daniel Gaiduk <daniel@gaiduk.dev>
2023-08-31 00:31:19 +0200

    Remove version

--- src/routes/(localized)/[[locale]]/+page.svelte
+++ src/routes/(localized)/[[locale]]/+page.svelte
@@ -1,7 +1,5 @@
 <script>
 	import { t } from '$lib/translations'
-	import { version } from '$app/environment'
 </script>
 
 <h1 class="text-4xl uppercase">{$t('app.name')}</h1>
-<h2 class="text-xs uppercase">Version: {version}</h2>
--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,11 @@
-import { fileURLToPath } from 'url'
-import { readFileSync } from 'fs'
 import { vitePreprocess } from '@sveltejs/kit/vite'
 import adapter from '@sveltejs/adapter-node'
-
-const url = new URL('package.json', import.meta.url)
-const file = fileURLToPath(url)
-const json = readFileSync(file, 'utf8')
-const pkg = JSON.parse(json)
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter(),
-		version: {
-			name: pkg.version
-		}
+		adapter: adapter()
 	}
 }
 
