2da0a3cb12a473f447254b60f0e082d4d3ba0825
Daniel Gaiduk <daniel@gaiduk.dev>
2023-08-31 21:17:50 +0200

    Rewrite exports in constants

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,13 +2,11 @@ import * as APP_CONFIG from '$lib/config/app.json'
 import * as TRANSLATION_CONFIG from '$lib/config/translation.json'
 import to_kebab_case from '$lib/utils/to_kebab_case'
 
-const KEBAB_APP_NAME = to_kebab_case(APP_CONFIG.APP_NAME)
-const AUTH_COOKIE_NAME = `${KEBAB_APP_NAME}_auth`
-const LOCALE_COOKIE_NAME = `${KEBAB_APP_NAME}_locale`
+export const KEBAB_APP_NAME = to_kebab_case(APP_CONFIG.APP_NAME)
+export const AUTH_COOKIE_NAME = `${KEBAB_APP_NAME}_auth`
+export const LOCALE_COOKIE_NAME = `${KEBAB_APP_NAME}_locale`
 
-const CONFIG = {
+export const CONFIG = {
 	...APP_CONFIG,
 	...TRANSLATION_CONFIG
 }
-
-export { CONFIG, AUTH_COOKIE_NAME, KEBAB_APP_NAME, LOCALE_COOKIE_NAME }
