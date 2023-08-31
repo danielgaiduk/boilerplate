ef227dd0218aa7320b9fcdfe496fbf96510c1042
Daniel Gaiduk <daniel@gaiduk.dev>
2023-08-31 23:10:46 +0200

    Rewrite seo in layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,34 +1,29 @@
 <script lang="ts">
 	import '../app.css'
 	import { CONFIG } from '$lib/constants'
-	import { MetaTags } from 'svelte-meta-tags'
 	import { page } from '$app/stores'
 	import { t } from '$lib/translations'
 	import get_all_localized_paths from '$lib/utils/get_all_localized_paths'
 
-	let title = ''
-	let description = ''
+	let title = $t(CONFIG.APP_NAME)
+	let description = $t(CONFIG.DEFAULT_DESCRIPTION)
 	/* global AlternateLinks */
 	let paths: AlternateLinks[] = []
 
 	$: if ($page?.data) {
+		title = $t($page?.data?.title || CONFIG.APP_NAME)
 		description = $t($page?.data?.description || CONFIG.DEFAULT_DESCRIPTION)
 		paths = get_all_localized_paths($page?.data?.locale, $page?.url)
-		title = $t($page?.data?.title || CONFIG.APP_NAME)
 	}
 </script>
 
 <svelte:head>
+	<title>{title} | {CONFIG.APP_NAME}</title>
+	<meta name="description" content={description} />
+	<link rel="canonical" href={$page?.url?.href} />
 	{#each paths as { href, hreflang }}
 		<link rel="alternate" {hreflang} {href} />
 	{/each}
 </svelte:head>
-
-<MetaTags
-	{description}
-	{title}
-	canonical={$page?.url?.href}
-	titleTemplate={`%s | ${CONFIG.APP_NAME}`}
-/>
 
 <slot />
