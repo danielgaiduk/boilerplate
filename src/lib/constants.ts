d1a2459bd55990cc718eb01e8bedd374dd877708
Daniel Gaiduk <daniel@gaiduk.dev>
2023-09-07 18:23:52 +0200

    Remove config jsons

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,12 +1,11 @@
-import * as APP_CONFIG from '$lib/config/app.json'
-import * as TRANSLATION_CONFIG from '$lib/config/translation.json'
-import to_kebab_case from '$lib/utils/to_kebab_case'
+import { toKebabCase } from '$lib/utils'
 
-export const KEBAB_APP_NAME = to_kebab_case(APP_CONFIG.APP_NAME)
+export const APP_NAME = 'Boilerplate'
+export const DEFAULT_LOCALE = 'en'
+export const LOADERS = ['app', 'error', 'homepage']
+export const LOCALES = ['en', 'de']
+export const THEME_COLOR = '#000000'
+
+export const KEBAB_APP_NAME = toKebabCase(APP_NAME)
 export const AUTH_COOKIE_NAME = `${KEBAB_APP_NAME}_auth`
 export const LOCALE_COOKIE_NAME = `${KEBAB_APP_NAME}_locale`
-
-export const CONFIG = {
-	...APP_CONFIG,
-	...TRANSLATION_CONFIG
-}
