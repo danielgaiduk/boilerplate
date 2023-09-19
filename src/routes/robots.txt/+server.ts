8ec8f11ad711d1795deb80b0bb75d0a119e20e3f
Daniel Gaiduk <daniel@gaiduk.dev>
2023-09-19 22:25:29 +0200

    Change robot.txt

--- src/routes/robots.txt/+server.ts
+++ src/routes/robots.txt/+server.ts
@@ -1,9 +1,15 @@
-export const GET = async () => {
-	const text = ['User-agent: *', 'Allow: /']
+import { PUBLIC_ORIGIN } from '$env/static/public'
 
-	return new Response(text.join('\n'), {
-		headers: {
-			'Content-Type': 'text/plain'
-		}
-	})
+export const prerender = true
+
+export async function GET(): Promise<Response> {
+	const body = ['User-agent: *', 'Allow: /', '', `Sitemap: ${PUBLIC_ORIGIN}/sitemap.xml`]
+		.join('\n')
+		.trim()
+
+	const headers = {
+		'Content-Type': 'text/plain'
+	}
+
+	return new Response(body, { headers })
 }
