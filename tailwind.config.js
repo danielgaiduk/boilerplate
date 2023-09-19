dc430ac917196b58a9e37287553e43a413e85fcd
Daniel Gaiduk <daniel@gaiduk.dev>
2023-09-19 22:42:17 +0200

    Add Inter font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,17 @@ import aspectRatio from '@tailwindcss/aspect-ratio'
 import containerQueries from '@tailwindcss/container-queries'
 import forms from '@tailwindcss/forms'
 import typography from '@tailwindcss/typography'
+import defaultTheme from 'tailwindcss/defaultTheme'
 
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {
+			fontFamily: {
+				inter: ['Inter var', ...defaultTheme.fontFamily.sans]
+			}
+		}
 	},
 	plugins: [aspectRatio, containerQueries, forms, typography]
 }
