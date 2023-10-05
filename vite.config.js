9f447df2f4ab554daeac1b62ce1ecb989299b580
Daniel Gaiduk <daniel@gaiduk.dev>
2023-10-05 17:03:28 +0200

    Remove pem certificates

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,10 @@
 import { defineConfig } from 'vitest/config'
 import { sveltekit } from '@sveltejs/kit/vite'
-import fs from 'fs'
 
 /** @type {import('vitest/config').UserConfigExport} */
 export default defineConfig({
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
-	},
-	server: {
-		https: {
-			key: fs.readFileSync(`${__dirname}/../certificates/key.pem`),
-			cert: fs.readFileSync(`${__dirname}/../certificates/cert.pem`)
-		},
-		proxy: {}
 	}
 })
