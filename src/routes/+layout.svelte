d4a2471c6af30bb1fb1a7de49331bc5aea65bcbc
Daniel Gaiduk <daniel@gaiduk.dev>
2023-10-05 17:39:23 +0200

    Rewrite seo in layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,22 +1,15 @@
 <script lang="ts">
 	import '../app.css'
+
+	import { getAllLocalizedPaths } from '$lib/utils'
 	import { page } from '$app/stores'
 	import { t } from '$lib/translations'
-	import { getAllLocalizedPaths } from '$lib/utils'
-
-	$: canonical = $page?.url?.href
-	$: description = $t($page?.data?.description)
-	$: paths = getAllLocalizedPaths($page?.url)
-	$: title = $t($page?.data?.title)
-	$: title_partial = $t('app.title_partial')
-	$: title_template = `${title} | ${title_partial}`
 </script>
 
 <svelte:head>
-	<title>{title_template}</title>
-	<meta name="description" content={description} />
-	<link rel="canonical" href={canonical} />
-	{#each paths as { href, hreflang }}
+	<title>{$t($page?.data?.title)} | {$t('app.title_partial')}</title>
+	<meta name="description" content={$t($page?.data?.description)} />
+	{#each getAllLocalizedPaths($page?.url) as { href, hreflang }}
 		<link rel="alternate" {hreflang} {href} />
 	{/each}
 </svelte:head>
